4b5bf3bb2caa49763a30f7e85c579ebad7f9b408
willian <williandeliz03@gmail.com>
2026-02-23 23:57:47 -0300

    feat: im lost, but i added a NewTaskWindow for the upcoming feature of adding tasks. Need to remember to create new styles for the TaskWindow textbox

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Fokus.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Fokus.ViewModels
 {
@@ -11,9 +13,19 @@ namespace Fokus.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged; // evento que é acionado quando uma propriedade é alterada.
 
+        public ICommand IOpenNewWindow => new RelayCommand(OpenNewWindow); // propriedade que retorna um comando para abrir uma nova janela. O comando é criado usando a classe RelayCommand, que é uma implementação comum de ICommand.
+
+        private void OpenNewWindow()
+        {
+            NewTaskWindow newTaskWindow = new NewTaskWindow(); // cria uma nova instância da janela NewTaskWindow.
+            newTaskWindow.Show(); // exibe a janela NewTaskWindow na tela.
+        }
+
         protected virtual void OnPropertyChanged(string propertyName) // método que é chamado para notificar a view sobre mudanças nas propriedades do ViewModel.
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); // verifica se o evento PropertyChanged é nulo e, se não for, invoca o evento passando o nome da propriedade que foi alterada.
         }
     }
 }
+
+  
